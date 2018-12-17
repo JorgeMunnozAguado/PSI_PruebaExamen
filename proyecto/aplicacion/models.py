2be629f014afb7dbd4b025d35fe753ab0f9b51e9
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-12-17 12:26:22 +0100

    Basic files

---
+++ proyecto/aplicacion/models.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Modelo del paciente
+class paciente(models.Model):
+
+    nombreP = models.CharField( max_length=500, blank=False )
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super(paciente, self).save(*args, **kwargs)
+        
+    # class Meta:
+    #     verbose_name_plural = 'Paciente'
+    
+    def __str__(self):
+        return self.name
+
+
+# Modelo del medico
+class medico(models.Model):
+
+    nombreM = models.CharField( max_length=500, blank=False )
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super(medico, self).save(*args, **kwargs)
+        
+    # class Meta:
+    #     verbose_name_plural = 'Medico'
+    
+    def __str__(self):
+        return self.name
+
+
+# Modelo del la receta
+class receta(models.Model):
+
+    nombreM = models.CharField( max_length=500, blank=False )
+    models.ForeignKey(medico)
+    models.ForeignKey(paciente)
+
+    # def save(self, *args, **kwargs):
+    #     self.slug = slugify(self.name)
+    #     super(medico, self).save(*args, **kwargs)
+        
+    # class Meta:
+    #     verbose_name_plural = 'Receta'
+    
+    def __str__(self):
+        return self.name
+        
+        
+        
+        
