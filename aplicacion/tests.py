2650cc4325a5b0ff574f3b1c4ef8b30bf55c0cfb
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-12-18 11:16:23 +0100

    Try

--- aplicacion/tests.py
+++ aplicacion/tests.py
@@ -2,5 +2,40 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from aplicacion.models import receta, paciente, medico
+from django.urls import reverse
 
-# Create your tests here.
+
+APLICACION_RECETAS = 'recetas'
+
+class AnimalTestCase(TestCase):
+
+borre todos los medicos, pacientes y recetas
+cree el m´edico (1,’medico1’)
+cree el paciente (1,’paciente1’)
+cree la receta (1,1,1)
+cree la receta (2,1,1)
+cree la receta (3,1,1)
+cree la receta (4,1,1)
+acceda a la vista relacionada con el URL $PROJECT URL/aplicacion/receta/
+compruebe que las recetas devueltas son las correctas.
+
+
+    def setUp(self):
+    
+        medico.objects.all().delete()
+        paciente.objects.all().delete()
+        receta.objects.all().delete()
+    
+        medico.objects.create(nombreM="medico1")
+        paciente.objects.create(nombreP="paciente1")
+        
+        receta.objects.create(medico=medico, paciente=paciente)
+        receta.objects.create(medico=medico, paciente=paciente)
+        receta.objects.create(medico=medico, paciente=paciente)
+        receta.objects.create(medico=medico, paciente=paciente)
+
+    def test_aplicacion_recetas(self):
+        
+        response = self._client.get(reverse(APLICACION_RECETAS), follow=True)
+        print "Response: " + str(response.content)
