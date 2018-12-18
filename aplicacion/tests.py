f5292530cc705fe5e82498b67a82ad8aa7fe841d
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-12-18 02:49:08 -0800

    Example working

--- aplicacion/tests.py
+++ aplicacion/tests.py
@@ -4,38 +4,34 @@ from __future__ import unicode_literals
 from django.test import TestCase
 from aplicacion.models import receta, paciente, medico
 from django.urls import reverse
+from django.test import Client
 
+APLICACION_RECETAS = 'receta_list'
 
-APLICACION_RECETAS = 'recetas'
-
-class AnimalTestCase(TestCase):
-
-borre todos los medicos, pacientes y recetas
-cree el m´edico (1,’medico1’)
-cree el paciente (1,’paciente1’)
-cree la receta (1,1,1)
-cree la receta (2,1,1)
-cree la receta (3,1,1)
-cree la receta (4,1,1)
-acceda a la vista relacionada con el URL $PROJECT URL/aplicacion/receta/
-compruebe que las recetas devueltas son las correctas.
-
+class recetaTest(TestCase):
 
     def setUp(self):
+    
+        self._client = Client()
     
         medico.objects.all().delete()
         paciente.objects.all().delete()
         receta.objects.all().delete()
     
-        medico.objects.create(nombreM="medico1")
-        paciente.objects.create(nombreP="paciente1")
+        med = medico.objects.create(nombreM="medico1")
+        pac = paciente.objects.create(nombreP="paciente1")
         
-        receta.objects.create(medico=medico, paciente=paciente)
-        receta.objects.create(medico=medico, paciente=paciente)
-        receta.objects.create(medico=medico, paciente=paciente)
-        receta.objects.create(medico=medico, paciente=paciente)
+        receta.objects.create(medico=med, paciente=pac)
+        receta.objects.create(medico=med, paciente=pac)
+        receta.objects.create(medico=med, paciente=pac)
+        receta.objects.create(medico=med, paciente=pac)
 
     def test_aplicacion_recetas(self):
         
-        response = self._client.get(reverse(APLICACION_RECETAS), follow=True)
-        print "Response: " + str(response.content)
+        lista_recetas = list(receta.objects.all())[-3:]
+        
+        response = self._client.get(reverse(APLICACION_RECETAS))
+
+        for rec in lista_recetas:
+            self.assertIn('<tr>\n<th>'+str(rec.id)+'</th>\n<td>'+rec.paciente.nombreP+'</td>\n<td>'+rec.medico.nombreM+'</td>\n</tr>', str(response.content))
+            print ("    assert: %s"%str(rec.id))
